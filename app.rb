c133f5ff264e39a2955d8d025aacdc56d681763e
Nicci Sheets <niccisheets@aol.com>
2018-09-16 18:32:07 -0400

    halfway finished with the app, need to work on the encryption and phonebook page

--- app.rb
+++ app.rb
@@ -1,43 +1,67 @@
 require 'sinatra'
-require 'mysql2'
+require 'pg'
+require 'securerandom'
 
 load 'local_env.rb' if File.exist?('local_env.rb')
 
 enable :sessions
 
-client = Mysql2::Client.new(:host => ENV['DB_HOST'], :username => ENV['DB_USERNAME'], :database => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
+conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
+
+def prepare_statements(conn)
+	conn.prepare("ndb", "insert into user_info (uuid, user_id, user_pass) values($1, $2, $3)")
+	# conn.prepare("cons", "insert into contact_info (names, address, phone, owner) values($1, $2, $3, $4)")
+end
+prepare_statements(conn)
 
 get '/' do
 	username = username || ""
 	password = password || ""
 	password2 = password2 || ""
-	erb :login, locals:{username: "", password: "", password2: ""}
+	erb :login
 end
 
 post '/new_user' do
 	username = params[:username]
 	password = params[:password]
 	password2 = params[:password2]
-	puts params
-	redirect '/phonebook?username=' + username + '&password=' + password + '&password2=' + password2
+
+	if password != password2
+		redirect '/'
+	end
+
+	conn.exec_prepared('ndb', [SecureRandom.uuid, params[:username] , params[:password]])
+
+	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}' AND user_pass = '#{password}' ")
+
+	res.each do |n|
+		session[:id] = n['uuid']
+		session[:user_name] = n['user_id']
+		redirect '/phonebook'
+	end
 end
 
 post '/existing_user' do
 	username = params[:username]
 	password = params[:password]
-	puts params
-	redirect '/phonebook?username=' + username + '&password=' + password
+
+	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}' AND user_pass = '#{password}' ")
+
+	res.each do |n|
+		session[:id] = n['uuid']
+		session[:user_name] = n['user_id']
+		redirect '/phonebook'
+	end
 end
 
 get '/phonebook' do
 	erb :phonebook
 end
 
-# post '/phonebook' do
-# end
 
+get '/sessions/logout' do
+	session.clear
+	redirect '/'
+end
 
-
-# post '/phonebook' do
-# end
 
