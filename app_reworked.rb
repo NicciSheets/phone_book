5efc38d23abc715cb7b107ee4ad6b333c8b46d29
Nicci Sheets <niccisheets@aol.com>
2018-10-03 03:27:43 -0400

    got error messages to come up when username is taken and when passwords do not match.

--- app_reworked.rb
+++ app_reworked.rb
@@ -10,26 +10,53 @@ enable :sessions
 
 get '/' do
 	message = message || ""
+	error_msg = error_msg || ""
+	erb :login, locals:{message: message, error_msg: error_msg}
+end
 
-	erb :login, locals:{message: message}
+get '/error' do
+	error_msg = error_msg || ""
+	erb :login, locals:{error_msg: error_msg}
+end
+
+post '/error' do
+	error_msg = error_msg || ""
+	erb :login, locals:{error_msg: error_msg}
 end
 
 post '/new_user' do
 	username = params[:username]
 	password = params[:password]
+	password2 = params[:password2]
 	my_password = BCrypt::Password.create(password)
 	uuid = SecureRandom.uuid
+	error_msg = ""
 	p "params in new_user is '#{params[:username]}' and the uuid is '#{uuid}'"
+
+	# if password != password2
+	# 	error_msg << "Passwords do not match, please try again."
+	# 	redirect '/error?error_msg=' + error_msg
+	# 	# erb :login, locals:{error_msg: error_msg}
+	# end
 
 	message = create_user_db(uuid, username, my_password)
 	
 	if message == "Username Already Taken"
-		message
-		redirect '/?message=' + message
+		error_msg << "Username already taken, please try again."
+		erb :login, locals:{error_msg: error_msg}
+	elsif password != password2
+		error_msg << "Passwords do not match, please try again."
+		erb :login, locals:{error_msg: error_msg}
 	else
 		redirect '/phonebook?uuid=' + uuid 
 	end
 end
+
+post '/existing_user' do
+
+
+end
+
 
 get '/phonebook' do
 	erb :phonebook
--- db_functions.rb
+++ db_functions.rb
@@ -4,6 +4,7 @@ require 'bcrypt'
 
 load 'local_env.rb' if File.exist?('local_env.rb')
 
+# checks new username against all usernames in db, if username is not already taken, it allows user to pass information to user_info database for username and password
 def create_user_db(uuid, username, my_password)
 	conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
 	check = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}'")
@@ -12,7 +13,12 @@ def create_user_db(uuid, username, my_password)
 		message = "Username Already Taken"
 	else
 		message = ""
-	conn.exec("INSERT INTO user_info (uuid, user_id, user_pass) VALUES ('#{uuid}', '#{username}', '#{my_password}')")
+		conn.exec("INSERT INTO user_info (uuid, user_id, user_pass) VALUES ('#{uuid}', '#{username}', '#{my_password}')")
 	end
 	message
+end
+
+def signin_existing_user_db(username, my_password)
+	conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
+		
 end
