be87a9881b4d6c10f10761ddbead9793e0ee8bd3
Nicci Sheets <niccisheets@aol.com>
2018-10-04 06:40:44 -0400

    fixed problem with the signup user, now all 3 instances work - username valid, password not valid, and username not valid

--- app_reworked.rb
+++ app_reworked.rb
@@ -25,71 +25,75 @@ end
 
 post '/new_user' do
 	username = params[:username]
+	# username.gsub!(/[^a-z0-9_\.]/, '') 
 	password = params[:password]
+	# password.gsub!(/[^0-9A-Za-z]/, '') 
 	password2 = params[:password2]
-	my_password = BCrypt::Password.create(password)
-	uuid = SecureRandom.uuid
-	
-	p "params in new_user is '#{params[:username]}' and the uuid is '#{uuid}'"
+	# password2.gsub!(/[^0-9A-Za-z]/, '') 
+	uuid = SecureRandom.uuid 
+	p "uuid is #{uuid}"
+	error_msg = ""
+	my_password = ""
+	if password == password2
+		my_password << BCrypt::Password.create(password)
+	else
+		my_password = ""
+	end
+	p "my password is #{my_password}"
 
 	db_func = create_user_db(uuid, username, my_password)
 	p "db_func is #{db_func}"
-	
+
 	error_msg = ""
 	if db_func == "Username Already Taken"
-		error_msg << "Username already taken, please try again."
+		error_msg << "Username already in use, please try again."
 		erb :login, locals:{error_msg: error_msg}
-	elsif password != password2
+	elsif db_func == "Passwords Don't Match"
 		error_msg << "Passwords do not match, please try again."
 		erb :login, locals:{error_msg: error_msg}
-	else
-		session[:owner] = uuid
-		redirect '/phonebook' 
+	else		
+		redirect '/phonebook?uuid=' + uuid	
 	end
 end
 
+
+# need to take the bcrypt off
 post '/existing_user' do
 	username = params[:username]
 	password = params[:password]
-	password_hash = BCrypt::Password.create(password)
-   	p "password_hash is #{password_hash} and its class is #{password_hash.class}"
-   	my_password =  BCrypt::Password.new(password_hash)
+	
    	p "my_password is #{my_password}"
-
-
+   	owner = params[:uuid]
+   	p "uuid is #{owner}"
 	error_msg = ""
-	db_func = signin_existing_user_db(username, my_password)
+	db_func = signin_existing_user_db(username, password)
 	p "db_func is #{db_func}"
-	# if db_func == "Correct Username" && my_password == password 
-	# 	redirect '/phonebook'
-	# elsif db_func == "Correct Username" && my_password != password
-	# 	error_msg << "Incorrect Password"
-	# 	erb :login, locals:{error_msg: error_msg}
-	# elsif db_func == "Incorrect Username" && my_password == password
-	# 	error_msg << "Incorrect Username"
-	# 	erb :login, locals:{error_msg: error_msg}
-	# elsif db_func == "Incorrect Username" && my_password != password
-	# 	error_msg << "Incorrect Username and Password"
-	# 	erb :login, locals:{error_msg: error_msg}
-	# end
 	if db_func == "Correct Username" 
 		if my_password == password 
-			redirect '/phonebook'
+			redirect '/phonebook&owner=' + owner
 		else
 			error_msg << "Incorrect Password"
 		end
 	else
-		if my_password == password
-			error_msg << "Incorrect Username"
-			erb :login, locals:{error_msg: error_msg}
-		else
-			error_msg << "Incorrect Username and Password"
-			erb :login, locals:{error_msg: error_msg}
-		end
+		error_msg << "Incorrect Username and Password"
+		erb :login, locals:{error_msg: error_msg}
 	end
 end
 
-
 get '/phonebook' do
-	erb :phonebook
+	names = names || ""
+	phone = phone || ""
+	address = address || ""
+	owner = params[:uuid]
+	id = session[:id]
+
+	p "owner is #{owner} and the sessions are #{params[:uuid]}"
+
+	phonebook_res = phonebook_table(owner)
+	res_arr = []
+	phonebook_res.each do |r|
+		res_arr << r
+	end
+
+	erb :phonebook, locals:{res_arr: res_arr, id: id, owner: owner, names: names, phone: phone, address: address}
 end
--- db_functions.rb
+++ db_functions.rb
@@ -4,23 +4,38 @@ require 'bcrypt'
 
 load 'local_env.rb' if File.exist?('local_env.rb')
 
+
+
+conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
+
+conn.prepare("cons", "insert into contacts (names, phone, address, owner) values($1, $2, $3, $4)")
+
+
 # checks new username against all usernames in db, if username is not already taken, it allows user to pass information to user_info database for username and password
 def create_user_db(uuid, username, my_password)
 	conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
-	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}'")
+	conn.prepare("ndb", "insert into user_info (uuid, user_id, user_pass) values($1, $2, $3)")
 	message = ""
+	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}'")
 	if res.num_tuples.zero? == false
 		message = "Username Already Taken"
 	else
-		message = ""
-		conn.exec("INSERT INTO user_info (uuid, user_id, user_pass) VALUES ('#{uuid}', '#{username}', '#{my_password}')")
+		if my_password == ""
+			message = "Passwords Don't Match"
+		else 
+			message = "Valid Username"
+			conn.exec_prepared('ndb', [uuid, params[:username], my_password])
+		end
 	end
 	message
 end
 
+
+# supposed to check username for existing user against database and somehwo incorporate the password for existing user, also
+# ******This is not working correctly, probably need to creata a User class and do stuff with that.
 def signin_existing_user_db(username, my_password)
 	conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
-	res = conn.exec("SELECT * FROM user_info WHERE user_id = '#{username}'")
+	res = conn.exec("SELECT user_id, user_pass FROM user_info WHERE user_id = '#{username}'")
 	message = ""
 	if res.num_tuples.zero? == true
 		message = "Incorrect Username"
@@ -28,17 +43,9 @@ def signin_existing_user_db(username, my_password)
 		message = "Correct Username"
 	end
 	message
-	p "message from the db functions is #{message}"
+end
 
-	# 	check_val = res.values.flatten
-	# 	p "check_val is #{check_val} its class is #{check_val.class} and its length is #{check_val.length}"
-
-	# 	if check_val[2] == "#{my_password}"
-	# 		true
-	# 	else
-	# 		false 
-	# 	end
-	# else
-	# 	false
-	# end
+def phonebook_table()
+	conn = PG::Connection.open(:host => ENV['DB_HOST'], :user => ENV['DB_USERNAME'], :dbname => ENV['DB_NAME'], :port => ENV['DB_PORT'], :password => ENV['DB_PASSWORD'])
+	conn.exec("SELECT * FROM contacts WHERE owner = '#{session[:uuid]}' ")
 end
